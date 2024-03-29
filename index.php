490a84e7cddd8c024b859902ef36d7261b2ce9b0
shamsazarfane <shams-dyn.azarfane@etu.u-bordeaux.fr>
2024-03-29 09:55:30 +0100

    update header

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ include('header.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="style/header_menu.css">
     <title>Bannière avec navigation</title>
@@ -18,32 +17,36 @@ include('header.php');
 <body>
 
     <div class="header">
+
         <div class="logo">
             <img src="img/logo.png" alt="Logo SQL CHALLENGER">
         </div>
-        <div class="header-title">
+
+        <div class="header-title text-left">
             <a href="index.php" style="text-decoration: none; color: inherit;">SQL CHALLENGER</a>
         </div>
 
-        <div class="header-links">
-            <?php
-            // Affichez les liens de connexion/inscription ou de données du compte/déconnexion en fonction de la connexion de l'utilisateur
-            if (isset($_SESSION['username'])) {
+        <div class="header-content">
+            <div class="header-links">
+                <?php
+                // Affichez les liens de connexion/inscription ou de données du compte/déconnexion en fonction de la connexion de l'utilisateur
+                if (isset($_SESSION['username'])) {
 
-                // Affichez la photo de profil si le chemin est disponible
-                if (!empty($userData['photo_path'])) {
-                    echo '<img src="' . $userData['photo_path'] . '" alt="Photo de profil" class="profile-photo">';
+                    // Affichez la photo de profil si le chemin est disponible
+                    if (!empty($userData['photo_path'])) {
+                        echo '<img src="' . $userData['photo_path'] . '" alt="Photo de profil" class="profile-photo">';
+                    }
+                    echo '<a href="account.php">' . $username . '</a>';
+                    if ($userData['admin']) {
+                        echo '<a href="back_office.php">Admin</a>';
+                    }
+                    echo '<a href="logout.php">Déconnexion</a>';
+                } else {
+                    echo '<a href="connexion.php">Connexion</a>';
+                    echo '<a href="inscription.php">Inscription</a>';
                 }
-                echo '<a href="account.php">' . $username . '</a>';
-                if ($userData['admin']) {
-                    echo '<a href="back_office.php">Admin</a>';
-                }
-                echo '<a href="logout.php">Déconnexion</a>';
-            } else {
-                echo '<a href="connexion.php">Connexion</a>';
-                echo '<a href="inscription.php">Inscription</a>';
-            }
-            ?>
+                ?>
+            </div>
         </div>
     </div>
 
