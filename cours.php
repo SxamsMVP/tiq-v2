9a5f2982a721c875e029aa5447ec5c0cdb5ce54a
Marion Faidy <marion.faidy@etu.u-bordeaux.fr>
2024-04-08 11:21:48 +0200

    update cours

--- cours.php
+++ cours.php
@@ -89,7 +89,7 @@ include('header.php');
         <h1 class="center-title">Apprendre le SQL</h1>
         <p>Le SQL, ou Structured Query Language, est un langage utilisé pour interagir avec les bases de données. Principalement adopté par les développeurs web, il permet de manipuler les données d'un site internet.</p>
         <h1 class="center-title">Cours</h1>
-        <p>Les cours sont conçus pour vous enseigner les commandes SQL essentielles telles que SELECT, INSERT INTO, UPDATE, DELETE, DROP TABLE, entre autres. Chaque commande est expliquée à travers des exemples clairs et succincts, offrant ainsi une formation pratique. En complément de la liste des commandes SQL, les cours proposent également des fiches mnémotechniques décrivant les fonctions SQL telles que AVG(), COUNT(), MAX(), etc. Ces ressources sont précieuses pour renforcer votre maîtrise du SQL.</p>
+        <p>Les cours sont conçus pour vous enseigner les commandes SQL essentielles telles que <code>SELECT, INSERT INTO, UPDATE, DELETE, DROP TABLE,</code> entre autres. Chaque commande est expliquée à travers des exemples clairs et succincts, offrant ainsi une formation pratique. En complément de la liste des commandes SQL, les cours proposent également des fiches mnémotechniques décrivant les fonctions SQL telles que <code>AVG(), COUNT(), MAX(),</code> etc. Ces ressources sont précieuses pour renforcer votre maîtrise du SQL.</p>
     </article>
     <img src="img/logoCours.png" alt="logo SQL">
 </div>
@@ -135,8 +135,8 @@ include('header.php');
                 <br>
                 <p>La syntaxe fondamentale de cette commande est la suivante :</p>
                 <p class="sql-command">SELECT nom_du_champ FROM nom_du_tableau</p>
-                <p>Cette requête SQL va sélectionner (SELECT) le champ "nom_du_champ" provenant (FROM) du tableau appelé "nom_du_tableau".</p>
-                <p>Le SQL, ou Structured Query Language, est un langage utilisé pour interagir avec les bases de données. La commande SELECT est l'une des commandes fondamentales de SQL et est utilisée pour récupérer des données à partir d'une ou plusieurs tables de base de données.</p>
+                <p>Cette requête SQL va sélectionner <code>SELECT</code> le champ <code>nom_du_champ</code> provenant <code>FROM</code> du tableau appelé <code>nom_du_tableau</code>.</p>
+                <p>Le SQL, ou Structured Query Language, est un langage utilisé pour interagir avec les bases de données. La commande <code>SELECT</code> est l'une des commandes fondamentales de SQL et est utilisée pour récupérer des données à partir d'une ou plusieurs tables de base de données.</p>
                 <p>Voici un exemple :</p>
                 <div class="table-responsive">
                     <table class="table table-bordered">
@@ -219,14 +219,14 @@ include('header.php');
         contentElement.innerHTML = `
             <article id="post-11" class="post-11 page type-page status-publish hentry">
                 <h1>SELECT DISTINCT</h1>
-                <p>L'usage de la commande SELECT en SQL permet de récupérer toutes les données d'une ou plusieurs colonnes. Cependant, cette commande peut entraîner l'affichage de lignes en doublon. Pour prévenir ces redondances dans les résultats, il suffit d'ajouter DISTINCT après SELECT.</p>
+                <p>L'usage de la commande <code>SELECT</code> en SQL permet de récupérer toutes les données d'une ou plusieurs colonnes. Cependant, cette commande peut entraîner l'affichage de lignes en doublon. Pour prévenir ces redondances dans les résultats, il suffit d'ajouter <code>DISTINCT</code> après <code>SELECT</code>.</p>
                 <br>
                 <p>La commande de base consiste à exécuter la requête suivante :</p>
                 <p class="sql-command">SELECT DISTINCT ma_colonne FROM nom_du_tableau</p>
-                <p>Cette requête récupère le champ "ma_colonne" de la table "nom_du_tableau" tout en évitant de renvoyer des doublons.</p>
+                <p>Cette requête récupère le champ <code>ma_colonne</code> de la table <code>nom_du_tableau</code> tout en évitant de renvoyer des doublons.</p>
 
                 <h2>Exemple</h2>
-                <p>Prenons le cas concret d’une table “client” qui contient des noms et prénoms:</p>
+                <p>Prenons le cas concret d’une table <code>client</code> qui contient des noms et prénoms:</p>
                 <div class="table-responsive">
                     <table class="table table-bordered">
                         <thead>
@@ -266,7 +266,7 @@ include('header.php');
                     </table>
                 </div>
 
-                <p>En utilisant seulement SELECT tous les noms sont retournés, or la table contient plusieurs fois le même prénom (cf. Pierre). Pour sélectionner uniquement les prénoms uniques il faut utiliser la requête suivante:</p>
+                <p>En utilisant seulement <code>SELECT</code> tous les noms sont retournés, or la table contient plusieurs fois le même prénom (cf. Pierre). Pour sélectionner uniquement les prénoms uniques il faut utiliser la requête suivante:</p>
                 <p class="sql-command">SELECT DISTINCT prenom FROM client</p>
                 <p>Cette requête va retourner les champs suivants:</p>
                 <div class="table-responsive">
@@ -292,7 +292,7 @@ include('header.php');
                         </tbody>
                     </table>
                 </div>
-                <p>Ce résultat affiche volontairement qu’une seule fois le prénom “Pierre” grâce à l’utilisation de la commande DISTINCT qui n’affiche que les résultats distincts.</p>
+                <p>Ce résultat affiche volontairement qu’une seule fois le prénom “Pierre” grâce à l’utilisation de la commande <code>DISTINCT</code> qui n’affiche que les résultats distincts.</p>
             </article>`;
         break;
 
@@ -300,9 +300,9 @@ include('header.php');
         contentElement.innerHTML = `
             <article id="post-11" class="post-11 page type-page status-publish hentry">
                 <h1>WHERE</h1>
-                <p>La clause WHERE dans une requête SQL sert à filtrer les lignes d'une base de données en fonction d'une condition spécifique. Elle permet ainsi de sélectionner uniquement les données qui répondent à ce critère, facilitant ainsi l'obtention des informations souhaitées.</p>
+                <p>La clause <code>WHERE</code> dans une requête SQL sert à filtrer les lignes d'une base de données en fonction d'une condition spécifique. Elle permet ainsi de sélectionner uniquement les données qui répondent à ce critère, facilitant ainsi l'obtention des informations souhaitées.</p>
                 <h2>Syntaxe</h2>
-                <p>La commande WHERE s’utilise en complément à une requête utilisant SELECT. La façon la plus simple de l’utiliser est la suivante:</p>
+                <p>La commande <code>WHERE</code> s’utilise en complément à une requête utilisant <code>SELECT</code>. La façon la plus simple de l’utiliser est la suivante:</p>
                 <p class="sql-command">SELECT nom_colonnes FROM nom_table WHERE condition</p>
                 <h2>Exemple</h2>
                 <p>Imaginons une base de données appelée “client” qui contient le nom des clients, le nombre de commandes qu’ils ont effectués et leur ville:</p>
@@ -392,24 +392,105 @@ include('header.php');
             </article>`;
         break;
 
-    case 'IN':
-        contentElement.innerHTML = `
-            <article id="post-11" class="post-11 page type-page status-publish hentry">
-                <h1>IN</h1>
-                <p>L’opérateur logique <code>IN</code> dans SQL s’utilise avec la commande <code>WHERE</code> pour vérifier si une colonne est égale à l'une des valeurs comprises dans un ensemble de valeurs déterminées. C’est une méthode simple pour vérifier si une colonne est égale à une valeur OU une autre valeur OU une autre valeur et ainsi de suite, sans avoir à utiliser de multiples fois l’opérateur <code>OR</code>.</p>
-                <h2>Syntaxe</h2>
-                <p>Pour chercher toutes les lignes où la colonne <code>nom_colonne</code> est égale à ‘valeur1’ OU ‘valeur2’ ou ‘valeur3’, il est possible d’utiliser la syntaxe suivante:</p>
-                <p class="sql-command">SELECT nom_colonne FROM table WHERE nom_colonne IN (valeur1, valeur2, valeur3, ...)</p>
-                <p>A savoir : entre les parenthèses il n’y a pas de limite du nombre d’arguments. Il est possible d’ajouter encore d’autres valeurs.</p>
-                <p>Cette syntaxe peut être associée à l’opérateur <code>NOT</code> pour rechercher toutes les lignes qui ne sont pas égales à l’une des valeurs stipulées.</p>
-            </article>`;
-        break;
+        case 'IN':
+                    contentElement.innerHTML = `
+                        <article id="post-11" class="post-11 page type-page status-publish hentry">
+                            <h1>IN</h1>
+                            <p>L’opérateur logique <code>IN</code> dans SQL s’utilise avec la commande <code>WHERE</code> pour vérifier si une colonne est égale à l'une des valeurs comprises dans un ensemble de valeurs déterminées. C’est une méthode simple pour vérifier si une colonne est égale à une valeur OU une autre valeur OU une autre valeur et ainsi de suite, sans avoir à utiliser de multiples fois l’opérateur <code>OR</code>.</p>
+                            <h2>Syntaxe</h2>
+                            <p>Pour chercher toutes les lignes où la colonne <code>nom_colonne</code> est égale à ‘valeur1’ OU ‘valeur2’ ou ‘valeur3’, il est possible d’utiliser la syntaxe suivante:</p>
+                            <p class="sql-command">SELECT nom_colonne FROM table WHERE nom_colonne IN (valeur1, valeur2, valeur3, ...)</p>
+                            <p>A savoir : entre les parenthèses il n’y a pas de limite du nombre d’arguments. Il est possible d’ajouter encore d’autres valeurs.</p>
+                            <p>Cette syntaxe peut être associée à l’opérateur <code>NOT</code> pour rechercher toutes les lignes qui ne sont pas égales à l’une des valeurs stipulées.</p>
+                        
+                            <p>Simplicité de l’opérateur <code>IN</code> La syntaxe utilisée avec l’opérateur est plus simple que d’utiliser une succession d’opérateur <code>OR</code>. Pour le montrer concrètement avec un exemple, voici 2 requêtes qui retournerons les mêmes résultats, l’une utilise l’opérateur <code>IN</code>, tandis que l’autre utilise plusieurs <code>OR</code>.</p>
+                            <p>Requête avec plusieurs <code>OR</code> :</p>
+                            <p class="sql-command">SELECT prenom FROM utilisateur WHERE prenom = 'Maurice' OR prenom = 'Marie' OR prenom = 'Thimoté'</p>
+                            <p>Requête équivalent avec l’opérateur <code>IN</code> :</p>
+                            <p class="sql-command">SELECT prenom FROM utilisateur WHERE prenom IN ('Maurice', 'Marie', 'Thimoté')</p>
+                        </article>`;
+                    break;
 
     case 'BETWEEN':
-        contentElement.innerHTML = '<article id="post-11" class="post-11 page type-page status-publish hentry"><h1>BETWEEN</h1><p>Contenu pour BETWEEN</p></article>';
-        break;
+    contentElement.innerHTML = `
+        <article id="post-11" class="post-11 page type-page status-publish hentry">
+            <h1>BETWEEN</h1>
+            <p>L'opérateur <code>BETWEEN</code> en SQL est utilisé pour sélectionner des valeurs dans une plage donnée. Il est souvent utilisé avec la clause <code>WHERE</code> pour filtrer les résultats en fonction de certaines conditions de plage.</p>
+            <h2>Syntaxe</h2>
+            <p>La syntaxe générale de l'opérateur <code>BETWEEN</code> est la suivante :</p>
+            <p class="sql-command">SELECT colonne FROM table WHERE colonne BETWEEN valeur1 AND valeur2</p>
+            <h2>Exemple</h2>
+            <p>Supposons que nous avons une table <code>produits</code> avec une colonne <code>prix</code> et nous voulons sélectionner tous les produits dont le prix est compris entre 10 et 50 euros :</p>
+            <p class="sql-command">SELECT * FROM produits WHERE prix BETWEEN 10 AND 50</p>
+            <p>Cette requête retournera tous les produits dont le prix est compris entre 10 et 50 euros, y compris les produits dont le prix est exactement 10 ou 50.</p>
+            <h2>Exemple : filtrer entre 2 dates</h2>
+<p>Imaginons une table <code>utilisateur</code> qui contient les membres d’une application en ligne :</p>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>nom</th>
+            <th>date_inscription</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Maurice</td>
+            <td>2012-03-02</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Simon</td>
+            <td>2012-03-05</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Chloé</td>
+            <td>2012-04-14</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Marie</td>
+            <td>2012-04-15</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Clémentine</td>
+            <td>2012-04-26</td>
+        </tr>
+    </tbody>
+</table>
+<p>Si l'on souhaite obtenir les membres qui se sont inscrits entre le 1 avril 2012 et le 20 avril 2012, il est possible d'effectuer la requête suivante :</p>
+<p class="sql-command">SELECT * FROM utilisateur WHERE date_inscription BETWEEN '2012-04-01' AND '2012-04-20'</p>
+<p>Résultat :</p>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>id</th>
+            <th>nom</th>
+            <th>date_inscription</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>3</td>
+            <td>Chloé</td>
+            <td>2012-04-14</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Marie</td>
+            <td>2012-04-15</td>
+        </tr>
+    </tbody>
+</table>
+        </article>`;
+    break;
+
+
     case 'LIKE':
-        contentElement.innerHTML = '<article id="post-11" class="post-11 page type-page status-publish hentry"><h1>LIKE</h1><p>Contenu pour LIKE</p></article>';
+        contentElement.innerHTML = '<article id="post-11" class="post-11 page type-page status-publish hentry"><h1>LIKE</h1><p>L\'opérateur <code>LIKE</code> est un élément de la clause <code>WHERE</code> utilisé dans les requêtes SQL. Il permet de rechercher des données basées sur un modèle spécifique. Par exemple, on peut rechercher des enregistrements où la valeur d\'une colonne commence par une lettre spécifique. Les modèles de recherche peuvent être variés.</p></article>';
         break;
     case 'GROUP BY':
         contentElement.innerHTML = '<article id="post-11" class="post-11 page type-page status-publish hentry"><h1>GROUP BY</h1><p>Contenu pour GROUP BY</p></article>';
